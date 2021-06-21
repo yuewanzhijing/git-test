42cbe25468f0955a30d13452e7f6698c28840d56
yuewanzhijing <81951185+yuewanzhijing@users.noreply.github.com>
2021-06-21 11:59:26 +0800

    remote changes

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("push test");
+         System.out.println("pull test");
     }
 }
